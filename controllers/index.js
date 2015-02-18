9748f25a33a3c08ef48eeba1602ef8906ef1bd6b
eplunk <erplunkett@gmail.com>
2015-02-18 05:27:58 +0000

    don't show events that are over

--- controllers/index.js
+++ controllers/index.js
@@ -2,15 +2,23 @@
 
 var events = require('../models/events');
 
+
 /**
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
+  var now = new Date();
   var contextData = {
     'title': 'MGT 656, the place 2 be ',
     'tagline': 'You are doomed (just kidding).',
-     'events':events.all
+    'events': []
   };
+  for(var i=0; i < events.all.length; i++){
+     var event = events.all[i];
+     if(event.date > now){
+       contextData.events.push(event);
+     }
+  }
   response.render('index.html', contextData);
 }
 
